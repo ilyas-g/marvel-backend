7edd6cb7a07779fa763c798a6c57f49b8e8bf6ca
Roddoark <ilyas150691@hotmail.fr>
2022-08-03 17:46:10 +0200

    Récupération des comics pour un seul personnage

--- routes/comic.js
+++ routes/comic.js
@@ -2,10 +2,10 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-router.get("/comic/:characterId", async (req, res) => {
+router.get("/comics/:characterId", async (req, res) => {
     console.log(req.params.characterId);
     try {
-        const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comic/${req.params.characterId}?apiKey=${process.env.API_KEY}`);
+        const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.API_KEY}`);
         // console.log(response.data);
         console.log("ID CHARACTER : " + req.params.name);
         res.json(response.data);
