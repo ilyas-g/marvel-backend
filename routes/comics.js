36594dff2b2ae1c7a27b2b32075edeea1f852a21
Roddoark <ilyas150691@hotmail.fr>
2022-08-03 22:49:54 +0200

    Clean code

--- routes/comics.js
+++ routes/comics.js
@@ -6,10 +6,7 @@ router.get("/comics", async (req, res) => {
     try {
         const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`);
         console.log("route log comics");
-
-        // console.log(response.data);
         res.json(response.data);
-
     } catch (error) {
         res.status(400).json(error.message);
     }
