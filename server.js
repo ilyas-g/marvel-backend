2b5203fdfce6fbbecfa5dbf5da5b6336f6d7e816
Roddoark <ilyas150691@hotmail.fr>
2022-08-04 10:42:31 +0200

    Instalation de morgan pour les logs

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require("express");
+const morgan = require("morgan");
 
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.json("Welcome on Marvel Api ! 🎉");
