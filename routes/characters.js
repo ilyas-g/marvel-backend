96bb3e6bda11af8c15971a6ce3832d70d2adf3a3
ilyas-g <ilyas15069i@gmail.com>
2022-10-08 17:45:29 +0200

    Fix de l'url de l'API avec les bons paramètres

--- routes/characters.js
+++ routes/characters.js
@@ -4,17 +4,19 @@ const router = express.Router();
 
 router.get("/characters", async (req, res) => {
     try {
-        const page = req.query;
-        const skip = page * 100;
-        let limit;
+
+        // const offset = page * 100 - 100;
 
         const response = await axios.get(
-            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&limit=${limit}&skip=${skip}`
+            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${req.query.skip}&limit=${req.query.limit}`
         );
-        // if (req.query.limit <= 10) req.query.limit = 10;
-        console.log("route log characters");
-        // console.log(response.data);
-        res.json(response.data);
+        if (req.query.limit < 1 || req.query.limit > 100) {
+            res.status(400).json({ message: "Limit must be between 1 and 100" });
+        } else {
+            console.log("route log characters");
+            // console.log(response.data);
+            res.json(response.data);
+        }
     } catch (error) {
         res.status(400).json(error.message);
     }
