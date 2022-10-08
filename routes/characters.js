b33f6405d2a0c2b3e9acd13fdf14214e79b5440e
ilyas-g <ilyas15069i@gmail.com>
2022-10-08 17:55:13 +0200

    Limit définie à 100

--- routes/characters.js
+++ routes/characters.js
@@ -6,17 +6,18 @@ router.get("/characters", async (req, res) => {
     try {
 
         // const offset = page * 100 - 100;
-
+        // let limit = req.query.limit;
+        // limit = 100;
         const response = await axios.get(
-            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${req.query.skip}&limit=${req.query.limit}`
+            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${req.query.skip}&limit=100`
         );
-        if (req.query.limit < 1 || req.query.limit > 100) {
-            res.status(400).json({ message: "Limit must be between 1 and 100" });
-        } else {
-            console.log("route log characters");
-            // console.log(response.data);
-            res.json(response.data);
-        }
+        // if (req.query.limit < 1 || req.query.limit > 100) {
+        //     res.status(400).json({ message: "Limit must be between 1 and 100" });
+        // } else {
+        console.log("route log characters");
+        // console.log(response.data);
+        res.json(response.data);
+        // }
     } catch (error) {
         res.status(400).json(error.message);
     }
